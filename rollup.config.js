213295c94e69858611222c9074e4bf488d8a62b6
babangsund <babangsund@gmail.com>
2019-08-14 00:32:49 +0200

    minify esm, license preamble

--- rollup.config.js
+++ rollup.config.js
@@ -8,62 +8,50 @@ import pkg from './package.json';
 const input = 'src/index.js';
 const globals = {react: 'React'};
 const external = Object.keys(globals).concat('@babel/runtime');
-const banner =
-  [
-    '/**',
-    ' * react-promiseportal v' + process.env.npm_package_version,
-    ' *',
-    ' * Copyright (c) 2019 babangsund',
-    ' *',
-    ' * This source code is licensed under the MIT license found in the',
-    ' * LICENSE file in the root directory of this source tree.',
-    ' */',
-  ].join('\n') + '\n';
+const license = {
+  output: {
+    preamble: [
+      '/**',
+      ' * react-promiseportal v' + process.env.npm_package_version,
+      ' *',
+      ' * Copyright (c) 2019 babangsund',
+      ' *',
+      ' * This source code is licensed under the MIT license found in the',
+      ' * LICENSE file in the root directory of this source tree.',
+      ' */',
+    ].join('\n'),
+  },
+};
 
-const base = {input, external};
-const output = {globals, banner};
-
-function makePlugins(minify, useESModules) {
-  return [
+export default {
+  input,
+  external,
+  plugins: [
     babel({
       runtimeHelpers: true,
       exclude: 'node_modules/**',
-      plugins: [['@babel/transform-runtime', {useESModules}]],
+      plugins: [['@babel/transform-runtime', {useESModules: true}]],
     }),
     nodeResolve(),
     commonjs(),
-    minify && terser(),
-  ];
-}
-const esm = {
-  ...base,
-  output: {
-    ...output,
-    format: 'esm',
-    file: pkg.module,
-  },
-  plugins: makePlugins(false, true),
+    process.env.NODE_ENV === 'production' && terser(license),
+  ],
+  output: [
+    {
+      globals,
+      format: 'esm',
+      file: pkg.module,
+    },
+    {
+      globals,
+      format: 'cjs',
+      file: pkg.main,
+    },
+    {
+      globals,
+      format: 'umd',
+      name: 'ReactPromisePortal',
+      file: `dist/index.umd.${process.env.NODE_ENV}.js`,
+    },
+  ],
 };
-
-const cjs = {
-  ...base,
-  output: {
-    ...output,
-    format: 'cjs',
-    file: pkg.main,
-  },
-  plugins: makePlugins(true, false),
-};
-
-const umd = {
-  ...base,
-  output: {
-    ...output,
-    format: 'umd',
-    name: 'ReactPromisePortal',
-    file: `dist/index.umd.${process.env.NODE_ENV}.js`,
-  },
-  plugins: makePlugins(process.env.NODE_ENV === 'production', true),
-};
-
-export default [umd, cjs, esm];
