5744683c27d7bbcec34dc896ae7a53220bc2d18b
babangsund <babangsund@gmail.com>
2019-11-07 13:58:20 +0100

    move index, enable coc

--- src/index.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-'use strict';
-
-export {default as PromisePortalProvider} from './PromisePortalProvider';
-export {default as PromisePortalContext} from './PromisePortalContext';
-export {default as withPromisePortal} from './withPromisePortal';
-export {default as usePromisePortal} from './usePromisePortal';
-export * from './PromisePortalTypes';
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from './PromisePortalTypes';
+export {default as usePromisePortal} from './usePromisePortal';
+export {default as withPromisePortal} from './withPromisePortal';
+export {default as PromisePortalContext} from './PromisePortalContext';
+export {default as PromisePortalProvider} from './PromisePortalProvider';
